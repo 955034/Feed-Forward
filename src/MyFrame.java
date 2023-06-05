080ddd988a29b3e5b9a7841c2c4cc8eb672503ba
mahme <mahme@10.0.0.117>
2023-06-05 19:42:22 -0400

    logo change

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-import java.awt.Image;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.*;
 import java.awt.image.*;
@@ -10,18 +11,23 @@ public class MyFrame extends JFrame {
 		this.setTitle("FeedForward");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setSize(2000,2000);
+		//this.setSize(2000,2000);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		pack();
+		setSize(screenSize.width,screenSize.height);
 		this.setVisible(true);
 		
-		this.getContentPane().setBackground(Color.white);
+		this.getContentPane().setBackground(new Color(212,220,222));
 		JLabel label = new JLabel();
-		label.setText("Introtext");
+		label.setText("Established 2023");
+		label.setAlignmentX(RIGHT_ALIGNMENT);
 		this.add(label);
-		ImageIcon logo = new ImageIcon("src/LogoForPage.png");
+		ImageIcon logo = new ImageIcon("src/FeedForward-1.png");
 		label.setIcon(logo);
-		//ImageIcon scaleImage = logo.getImage().getScaledInstance(208, 208, Image.SCALE_DEFAULT);
-		label.setHorizontalTextPosition(JLabel.CENTER);
-		label.setVerticalTextPosition(JLabel.CENTER);
-		label.setForeground(Color.green);
+		//label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.BOTTOM);
+		label.setForeground(new Color(75,141,151));
+		this.setIconImage(logo.getImage());
+
 	}
 }
